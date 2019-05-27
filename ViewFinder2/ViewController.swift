06dc1fa38e642da38e24c1c3447cb2f595374ba0
Colby Dickinson <colby.dickinson@cox.net>
2019-05-26 19:38:46 -0500

    Initial commit

---
+++ ViewFinder2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ViewFinder2
+//
+//  Created by Colby Dickinson on 5/26/19.
+//  Copyright © 2019 Colby Dickinson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
