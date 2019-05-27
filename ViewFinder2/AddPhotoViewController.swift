ef3cce383de03ffa5677e7ebb985be9a18c2e15d
Colby Dickinson <colby.dickinson@cox.net>
2019-05-26 20:29:42 -0500

    Complete Iteration 1

---
+++ ViewFinder2/AddPhotoViewController.swift
@@ -0,0 +1,56 @@
+//
+//  AddPhotoViewController.swift
+//  ViewFinder2
+//
+//  Created by Colby Dickinson on 5/26/19.
+//  Copyright © 2019 Colby Dickinson. All rights reserved.
+//
+
+import UIKit
+
+class AddPhotoViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    var imagePicker = UIImagePickerController()
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var textField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imagePicker.delegate = self
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func cameraTapped(_ sender: UIButton) {
+        imagePicker.sourceType = .camera
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+     @IBAction func photoLibraryTapped(_ sender: UIButton) {
+        imagePicker.sourceType = .photoLibrary
+        
+        present(imagePicker, animated: true, completion: nil)
+     }
+    
+    @IBAction func saveTapped(_ sender: Any) {
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.image = selectedImage
+        }
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    //     // MARK: - Navigation
+//
+//    // In a storyboard-based application, you will often want to do a little preparation before navigation
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        // Get the new view controller using segue.destination.
+//        // Pass the selected object to the new view controller.
+//    }
+//    */
+
+}
